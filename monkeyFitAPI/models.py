5a8adbda2b5c7601d5dc3c3303a524f0f8a05fb2
Sebastian Zanabria <zanabria.s@pucp.pe>
2020-12-02 15:48:15 -0500

    feat: :sparkles: create place model

--- monkeyFitAPI/models.py
+++ monkeyFitAPI/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    place_id = models.CharField(max_length=40) 
+    name = models.CharField(max_length=30, null=True) 
+    formatted_address = models.CharField(max_length=100, null=True)
+    business_status = models.CharField(max_length=30, null=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True)
+    url = models.CharField(max_length=100, null=True)
+    user_ratings_total = models.IntegerField(null=True)
+    utc_offset = models.IntegerField(null=True)
