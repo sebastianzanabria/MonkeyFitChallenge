21d382eebe188fcb6f12b8311224040bfe10ee93
Sebastian Zanabria <zanabria.s@pucp.pe>
2020-12-02 17:49:00 -0500

    fix: :bug: Enable fields max length equal to 100

--- monkeyFitAPI/models.py
+++ monkeyFitAPI/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Place(models.Model):
-    place_id = models.CharField(max_length=40) 
-    name = models.CharField(max_length=30, null=True) 
+    place_id = models.CharField(max_length=100) 
+    name = models.CharField(max_length=100, null=True) 
     formatted_address = models.CharField(max_length=100, null=True)
-    business_status = models.CharField(max_length=30, null=True)
-    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True)
+    business_status = models.CharField(max_length=100, null=True)
+    rating = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     url = models.CharField(max_length=100, null=True)
     user_ratings_total = models.IntegerField(null=True)
     utc_offset = models.IntegerField(null=True)
