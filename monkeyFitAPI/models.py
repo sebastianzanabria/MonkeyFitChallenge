d29db9e1b25fe171c21a9709fe6a5c9289af089e
Sebastian Zanabria <zanabria.s@pucp.pe>
2020-12-02 18:12:01 -0500

    fix: :bug: Enable char fields max len equal to 200

--- monkeyFitAPI/models.py
+++ monkeyFitAPI/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Place(models.Model):
-    place_id = models.CharField(max_length=100) 
-    name = models.CharField(max_length=100, null=True) 
-    formatted_address = models.CharField(max_length=100, null=True)
-    business_status = models.CharField(max_length=100, null=True)
+    place_id = models.CharField(max_length=200) 
+    name = models.CharField(max_length=200, null=True) 
+    formatted_address = models.CharField(max_length=200, null=True)
+    business_status = models.CharField(max_length=200, null=True)
     rating = models.DecimalField(max_digits=4, decimal_places=1, null=True)
-    url = models.CharField(max_length=100, null=True)
+    url = models.CharField(max_length=200, null=True)
     user_ratings_total = models.IntegerField(null=True)
     utc_offset = models.IntegerField(null=True)
